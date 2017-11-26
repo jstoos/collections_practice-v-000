7301b85ecb9cf2f90296f55980689e39c748c9eb
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-26 20:47:04 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -60,9 +60,8 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |word, index|
-    if index = 1
-      word = word
-    else word << "s"
+    if index != 1
+      word << "s"
     end
   end
 end
